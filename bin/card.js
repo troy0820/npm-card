6e314b8ceec8fc4b7e93e71e0e2d29b0bff06b75
Troy Connor <troy0820@gmail.com>
2022-04-08 17:02:58 -0400

    bin: changed job to MSFT

--- bin/card.js
+++ bin/card.js
@@ -14,18 +14,18 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white.bold('Troy Connor  /'),
-  handle: chalk.redBright.bold('troy0820'),
-  work: chalk.redBright.bold('Senior Software Engineer at Red Hat'),
-  twitter: chalk.redBright.bold('https://twitter.com/troy0820'),
-  github: chalk.redBright.bold('https://github.com/troy0820'),
-  linkedin: chalk.redBright.bold('https://www.linkedin.com/in/troyconnor/'),
-  web: chalk.redBright.bold('https://troy0820.dev'),
-  npx: chalk.redBright.bold('npx troy0820'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  name: chalk.hex('#464EB8').bold('Troy Connor  /'),
+  handle: chalk.hex('#464EB8').bold('troy0820'),
+  work: chalk.hex('#464EB8').bold('Senior Software Engineer at Microsoft'),
+  twitter: chalk.hex('#464EB8').bold('https://twitter.com/troy0820'),
+  github: chalk.hex('#464EB8').bold('https://github.com/troy0820'),
+  linkedin: chalk.hex('#464EB8').bold('https://www.linkedin.com/in/troyconnor/'),
+  web: chalk.hex('#464EB8').bold('https://troy0820.dev'),
+  npx: chalk.hex('#464EB8').bold('npx troy0820'),
+  labelWork: chalk.hex('#F25022').bold('      Work:'),
+  labelTwitter: chalk.hex('#7FBA00').bold('   Twitter:'),
+  labelGitHub: chalk.hex('#FFB900').bold('    GitHub:'),
+  labelLinkedIn: chalk.hex('#453EB8').bold('  LinkedIn:'),
   labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
@@ -43,4 +43,4 @@ const carding = `${data.labelCard}  ${data.npx}`
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
 
-console.log(chalk.redBright.bold(boxen(output, options)))
+console.log(chalk.hex('#464EB8').bold(boxen(output, options)))
